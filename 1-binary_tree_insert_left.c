34b79d3df85ae632c8cc36c29208dc56ad45a530
Mufidat-Ahmed <imufidatahmad@gmai.com>
2023-10-03 04:24:28 -0700

    1-left

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -4,7 +4,7 @@
  * binary_tree_insert_left - inserts a node as the left-child of another node
  * @parent: inserts a node as the left-child of another node
  * @value: inserts a node as the left-child of another node
- * Return: NULL or  new node on success 
+ * Return: NULL or  new node on success
  */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
