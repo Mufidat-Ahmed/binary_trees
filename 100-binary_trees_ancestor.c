4f9f6487f275d29b8a120b6cb14bc514890c43af
Mufidat-Ahmed <imufidatahmad@gmai.com>
2023-10-03 21:37:55 -0700

    100-ancestor

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -9,5 +9,44 @@
 
 binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
 {
+	binary_tree_t *first_path[1000], *second_path[1000], *current, *loc;
+	size_t len1 = 0, len2 = 0;
+
+	if (first == NULL || second == NULL)
+	{
+		return (NULL);
+	}
+
+	current = (binary_tree_t *)first;
+	while (current != NULL)
+	{
+		first_path[len1] = current;
+		current = current->parent;
+	}
+	current = (binary_tree_t *)second;
+	while (current != NULL)
+	{
+		second_path[len2] = current;
+		current = current->parent;
+	}
+	while (len1 > 0 && len2 > 0)
+	{
+		if (first_path[len1 - 1] != second_path[len2 - 1])
+		{
+			break;
+		}
+		len1--;
+		len2--;
+	}
+	if (len1 == 0)
+	{
+		return ((binary_tree_t *)first);
+	}
+	else if (len2 == 0)
+	{
+		return ((binary_tree_t *)second);
+	}
+	loc = first_path[len1];
+	return (loc);
 
 }
